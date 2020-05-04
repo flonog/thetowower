9cab5a6f737ce9faf2ff19b580985f332f5219c4
floleproto <39876308+floleproto@users.noreply.github.com>
2020-05-04 11:13:04 +0200

    Remove unused import

--- src/com/floleproto/thetower/gui/configmenu/time/TimeConfig.java
+++ src/com/floleproto/thetower/gui/configmenu/time/TimeConfig.java
@@ -8,7 +8,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class TimeConfig extends NumberConfigMenu {
     public TimeConfig(Player player) {
